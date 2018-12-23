6c7dff4c79afd3ae65d29131f4d208af637d416b
fedodd <fedodd.s@gmail.com>
2018-12-24 00:22:40 +0300

    programm

--- src/js/index.js
+++ src/js/index.js
@@ -4,10 +4,6 @@ import move from './vendor/move' */
 
 /* tabLinks, tabImages, tabs */
 
-const tabs = [...document.querySelectorAll('div.tab')];
-const tabLinks = [...document.querySelectorAll('a.menu-link')];
-
-menuTabs(tabLinks, tabs);
 
 $(document).ready(function () {
   $('.banner-sliders').slick({
@@ -24,30 +20,19 @@ $(document).ready(function () {
 });
 
 $(document).ready(function () {
-  $('.press').slick({
-    dots: false,
+  $('.program-slider').slick({
+    dots: true,
+    customPaging: function (slider, i) {
+      var thumb = $(slider.$slides[i]).data();
+      return '<a>' + (i+1) + '</a>';
+    },
     arrows: false,
+    infinite: true,
+    autoplay: false,
     pauseOnHover: false,
+    pauseOnDotsHover: true,
     speed: 500,
     autoplaySpeed: 5000,
-    infinite: false,
-    autoplay: false,
-    slidesToShow: 4,
-    responsive: [
-      {
-        breakpoint: 769,
-        settings: {
-          infinite: true,
-          autoplay: true,
-          slidesToShow: 1,
-          centerMode: true,
-          variableWidth: true
-        }
-      }
-      
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
-    ]
+    dotsClass: 'program-dots'
   });
 });
